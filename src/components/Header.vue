d11ac89c6527f0f914ff471f1b76d4ea6e6abd86
rohitjethoe <rohitchai@icloud.com>
2020-08-22 22:35:14 +0200

    Added header structure.

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="header">
+        <div class="logo">
+            Rohit Jethoe
+        </div>
+        <div class="nav">
+            <ul>
+                <li>
+                    <a href="#projects">
+                        projects
+                    </a>
+                </li>
+                <li>
+                    <a href="#experience">
+                        experience
+                    </a>
+                </li>
+                <li>
+                    <a href="#about">
+                        about
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="burger-icon">
+            <div class="bar bar-1"></div>
+            <div class="bar bar-2"></div>
+            <div class="bar bar-3"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
