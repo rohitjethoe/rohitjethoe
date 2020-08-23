3289eef42b84261952c56e9f8e5cdde016cbd216
rohitjethoe <rohitchai@icloud.com>
2020-08-23 15:08:04 +0200

    Added skills.

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,12 +3,190 @@
       <div class="section-title">
           My skills
       </div>
+      <div class="section-skills">
+        <div class="skill level-1">
+          <span class="type">
+            language:
+          </span>
+          <span class="technology">
+            html
+          </span>
+        </div>
+        <div class="skill level-1">
+          <span class="type">
+            language:
+          </span>
+          <span class="technology">
+            css
+          </span>
+        </div>
+        <div class="skill level-2">
+          <span class="type">
+            language:
+          </span>
+          <span class="technology">
+            javascript
+          </span>
+        </div>
+        <div class="skill level-0">
+          <span class="type">
+            language:
+          </span>
+          <span class="technology">
+            php
+          </span>
+        </div>
+        <div class="skill level-0">
+          <span class="type">
+            database:
+          </span>
+          <span class="technology">
+            mysql
+          </span>
+        </div>
+        <div class="skill level-2">
+          <span class="type">
+            editor:
+          </span>
+          <span class="technology">
+            vscode
+          </span>
+        </div>
+        <div class="skill level-2">
+          <span class="type">
+            version control:
+          </span>
+          <span class="technology">
+            git
+          </span>
+        </div>
+        <div class="skill level-1">
+          <span class="type">
+            platform:
+          </span>
+          <span class="technology">
+            github
+          </span>
+        </div>
+        <div class="skill level-2">
+          <span class="type">
+            package manager:
+          </span>
+          <span class="technology">
+            npm
+          </span>
+        </div>
+        <div class="skill level-2">
+          <span class="type">
+            runtime:
+          </span>
+          <span class="technology">
+            node
+          </span>
+        </div>
+        <div class="skill level-2">
+          <span class="type">
+            framework:
+          </span>
+          <span class="technology">
+            vuejs
+          </span>
+        </div>
+        <div class="skill level-0">
+          <span class="type">
+            framework:
+          </span>
+          <span class="technology">
+            react
+          </span>
+        </div>
+        <div class="skill level-0">
+          <span class="type">
+            framework:
+          </span>
+          <span class="technology">
+            react native
+          </span>
+        </div>
+        <div class="skill level-2">
+          <span class="type">
+            database:
+          </span>
+          <span class="technology">
+            mongodb
+          </span>
+        </div>
+        <div class="skill level-0">
+          <span class="type">
+            framework:
+          </span>
+          <span class="technology">
+            electronjs
+          </span>
+        </div>
+        <div class="skill level-1">
+          <span class="type">
+            library:
+          </span>
+          <span class="technology">
+            puppeteer
+          </span>
+        </div>
+        <div class="skill level-2">
+          <span class="type">
+            framework:
+          </span>
+          <span class="technology">
+            expressjs
+          </span>
+        </div>
+        <div class="skill level-0">
+          <span class="type">
+            hosting:
+          </span>
+          <span class="technology">
+            netlify
+          </span>
+        </div>
+        <div class="skill level-1">
+          <span class="type">
+            hosting:
+          </span>
+          <span class="technology">
+            heroku
+          </span>
+        </div>
+        <div class="skill level-1">
+          <span class="type">
+            philosophy:
+          </span>
+          <span class="technology">
+            web automation
+          </span>
+        </div>
+        <div class="skill level-1">
+          <span class="type">
+            philosophy:
+          </span>
+          <span class="technology">
+            responsive web development
+          </span>
+        </div>
+        <div class="skill level-2">
+          <span class="type">
+            version control:
+          </span>
+          <span class="technology">
+            terminal
+          </span>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'Skills'
 }
 </script>
 
