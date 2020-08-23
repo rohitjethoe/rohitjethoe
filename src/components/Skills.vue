f8188ecb827bee7d8117298050d929c33217eeb9
rohitjethoe <rohitchai@icloud.com>
2020-08-23 13:27:27 +0200

    New component.

---
+++ src/components/Skills.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="skills section" id="skills">
+      <div class="section-title">
+          My skills
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
