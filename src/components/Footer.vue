bd9cf5c46668e8dcf36a5cab57eaf1111c397810
rohitjethoe <rohitchai@icloud.com>
2020-08-23 13:26:24 +0200

    Footer.

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="footer">
+    Developed with <span class="footer-heart">❤</span> by Rohit.️ 
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style>
+    .footer {
+        text-align: center;
+        padding: 6px;
+    }
+
+    .footer-heart {
+        color: #db0000;
+    }
+</style>
