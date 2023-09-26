7bebc326219ea9968647944c903480294d5fa2fb
rohitjethoe <rohit@buildbyrelay.com>
2023-09-26 21:50:36 +0200

    Case updated

--- src/views/projects/Monostatic.vue
+++ src/views/projects/Monostatic.vue
@@ -4,6 +4,7 @@
     <CoverImage source="/monostatic-website.webp" project="1"/>
     <Images :images="images"/>
     <Workflow />
+    <TextWithImage :heading="textWithImage[0].heading" :text="textWithImage[0].text" :image="textWithImage[0].image"/>
     <FeaturedProjects :featuredProjects="featuredProjects" />
 </template>
 
@@ -13,6 +14,7 @@ import Music from '@/components/Music.vue';
 import ProjectDetails from '@/components/ProjectDetails.vue';
 import Images from '@/components/Images.vue';
 import FeaturedProjects from '@/components/FeaturedProjects.vue';
+import TextWithImage from '@/components/TextWithImage.vue';
 import Workflow from '@/components/Workflow.vue';
 
 export default {
@@ -35,6 +37,13 @@ export default {
                     start: 8,
                 }
             },
+            textWithImage: {
+                0: {
+                    heading: "Monostatic: Accelerating ventures by investing design and innovation",
+                    text: "I joined Monostatic as the first employee of their new development studio, Build By Relay, kick-starting my career in web development while pursuing my dream of enrolling in the TU Delft Computer Science Bachelor program.<br /><br />Working at Monostatic taught me numerous valuable skills, including creating structured code and following a workflow. Additionally, I learned how to develop outstanding applications as part of a team. Notably, at Monostatic, I not only improved as a developer but also gained insights into entrepreneurship, critical thinking in design, achieving perfection, and various other valuable skills.<br /><br />I have been with Monostatic since March \'23 and continue to work here today.",
+                    image: "/monostatic-door.webp"
+                }
+            },
             featuredProjects: {
                 0: {
                     path: '/relay-square.webp',
@@ -49,7 +58,7 @@ export default {
             }
         }
     },
-    components: { CoverImage, ProjectDetails, Music, Images, Workflow, FeaturedProjects }
+    components: { CoverImage, ProjectDetails, Music, Images, Workflow, TextWithImage,  FeaturedProjects }
 }
 </script>
 
