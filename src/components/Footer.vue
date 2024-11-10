01ea6053f9c858a3437b35712076824cabdb1dda
Rohit Jethoe <rohitjethoe@proton.me>
2024-11-10 18:02:14 +0100

    footer structure

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,6 +3,21 @@
 
 <template>
     <footer class="qa-footer">
-        
+        <div class="qa-container">
+            <div class="qa-footer__slug">
+            
+            </div>
+            <div class="qa-footer__socials">
+
+            </div>
+        </div>
+        <div class="qa-container">
+            <div class="qa-footer__copyright">
+
+            </div>
+            <div class="qa-footer__spotify">
+                
+            </div>
+        </div>
     </footer>
 </template>
