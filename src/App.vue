2e4da53cbfa7947f9f279cf59f0c7fa39293aaf8
Rohit Jethoe <rohitjethoe@proton.me>
2024-11-10 15:38:17 +0100

    migrate App.vue contents to Home.vue

--- src/App.vue
+++ src/App.vue
@@ -6,41 +6,7 @@ import Footer from '@/components/Footer.vue';
 <template>
   <Header />
   <main class="qa-main">
-    <section class="qa-introductory">
-      <div class="qa-container">
-        <div class="qa-introductory__slug">
-          Crafting pixel-perfect digital experiences, as a full-stack Developer besides studying mathematics & engineering methods.        
-        </div>
-        <div class="qa-introductory__major">
-          (BSc) Computer Science and Engineering @ T<span>U</span> Delft<br />
-        </div>
-        <div class="qa-introductory__timeline">
-          September 2024 - <span>now</span>
-        </div>
-        <div class="qa-introductory__experience">
-          Full-stack Developer at Monostatic Studio <img src="@/assets/monostatic.png" />
-        </div>
-        <div class="qa-introductory__timeline">
-          March 2023 - <span>August 2024</span>
-        </div>
-      </div>
-    </section>
+    <router-view></router-view>
   </main>
   <Footer />
 </template>
-
-<style lang="scss" scoped>
-.qa-introductory {
-	font-family: "Projekt Blackbird";
-
-	&__slug {
-		color: #8A8A8A;
-	}
-
-	&__major {
-		span {
-			color: #00AEE3;
-		}
-	}
-}
-</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,5 +2,66 @@
 </script>
 
 <template>
-    <div></div>
+    <section class="qa-introductory">
+      <div class="qa-container">
+        <div class="qa-introductory__slug">
+          Crafting pixel-perfect digital experiences, as a full-stack Developer besides studying mathematics & engineering methods.        
+        </div>
+        <div class="qa-introductory__major">
+          (BSc) Computer Science and Engineering @ T<span>U</span> Delft<br />
+        </div>
+        <div class="qa-introductory__timeline">
+          September 2024 - <span>now</span>
+        </div>
+        <div class="qa-introductory__experience">
+          Full-stack Developer at Monostatic Studio <img src="@/assets/monostatic.png" />
+        </div>
+        <div class="qa-introductory__timeline">
+          March 2023 - <span>August 2024</span>
+        </div>
+      </div>
+    </section>
+
+	<section class="qa-contact">
+		<div class="qa-container">
+			<div class="qa-contact__email">
+				<a class="qa-contact__link" href="mailto:rohitjethoe@proton.me">
+					rohitjethoe@proton.me
+				</a>
+			</div>
+			<div class="qa-contact__github">
+				<a class="qa-contact__link" href="https://github.com/rohitjethoe" target="_blank">
+					github.com/<span>rohitjethoe</span>
+				</a>
+			</div>
+		</div>
+	</section>
 </template>
+
+<style lang="scss" scoped>
+@use "@/styles/functions" as *;
+
+.qa-introductory {
+	font-family: "Projekt Blackbird";
+	font-size: math-clamp(19, 25.333);
+
+	&__slug {
+		margin-bottom: math-clamp(30, 40);
+		color: #8A8A8A;
+	}
+
+	&__major {
+		span {
+			color: #00AEE3;
+		}
+	}
+
+	&__timeline {
+		margin-bottom: math-clamp(30, 40);
+	}
+
+	&__major, &__experience {
+		margin-bottom: math-clamp(8, 12);
+	}
+}
+</style>
