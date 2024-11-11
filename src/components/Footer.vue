17763aff4014fe05820eec802bef2a7300cccf4e
Rohit Jethoe <rohitjethoe@proton.me>
2024-11-11 12:25:50 +0100

    added footer & replace huistuin cover

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,23 +1,129 @@
 <script setup>
+import { onMounted, ref } from 'vue';
+
+const playingNowData = ref(null);
+
+onMounted(() => {
+    const clientId = 'd39a4a65f68849c792163cb28588b585';
+    const clientSecret = 'e133cd0cb2914c26ba642744f9dfdb21';
+    const refresh_token = 'AQDPWIm3XwP8JhRQKDVjXeI44zM2tse16DddlKqD4pJm_bPHeztriPFoA7n1NzI6uqf6-5OWf_T_Tvjb1_P8qsGgVYynbnY64mZLxaoDKqZVfmji5l0jX8gbz4HAdpq8IHU';
+    const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token';
+
+    const getAuthToken = async () => {
+        let response = await fetch("https://accounts.spotify.com/api/token", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                Authorization: "Basic " + btoa("d39a4a65f68849c792163cb28588b585:e133cd0cb2914c26ba642744f9dfdb21")
+            },
+            body: new URLSearchParams({
+                grant_type: "refresh_token",
+                refresh_token: "AQDPWIm3XwP8JhRQKDVjXeI44zM2tse16DddlKqD4pJm_bPHeztriPFoA7n1NzI6uqf6-5OWf_T_Tvjb1_P8qsGgVYynbnY64mZLxaoDKqZVfmji5l0jX8gbz4HAdpq8IHU"
+            }).toString()
+        });
+
+        let data = await response.json();
+        return data.access_token;
+    }
+
+    const getPlayingNow = async (bearer) => {
+        try {
+            let response = await fetch("https://api.spotify.com/v1/me/player/currently-playing", {
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${bearer}`
+                }
+            });
+
+            const result = await response.json();
+            console.log(result);
+
+            return result;
+        } catch(err) {
+            return null;
+        }
+    }
+
+    (async() => {
+        let bearer = await getAuthToken();
+        playingNowData.value = await getPlayingNow(bearer);
+        
+        if (playingNowData.value !== null) {
+            document.querySelector('.qa-footer__song').innerHTML = await playingNowData.value.item.name;
+            document.querySelector('.qa-footer__artist').innerHTML = await playingNowData.value.item.artists[0].name;
+            document.querySelector('.qa-footer__link').href = await playingNowData.value.item.external_urls.spotify;
+        } else {
+            document.querySelector('.qa-footer__song').innerHTML = 'not playing currently...';
+            document.querySelector('.qa-footer__divider').innerHTML = '';
+        }
+    })();
+})
 </script>
 
 <template>
     <footer class="qa-footer">
         <div class="qa-container">
             <div class="qa-footer__slug">
-            
+                LET’S TALK<br />
+                ABOUT THE NEXT<br />
+                BIG THING.
             </div>
             <div class="qa-footer__socials">
 
             </div>
         </div>
-        <div class="qa-container">
+        <div class="qa-container qa-footer__bottom">
             <div class="qa-footer__copyright">
-
+                © 2024 - Rohit Jethoe
             </div>
             <div class="qa-footer__spotify">
-                
+                <a class="qa-footer__link" href="https://spotify.com/rohitjethoe" target="_blank">
+                    <img src="@/assets/spotify.svg" alt="spotify"/>
+                    <span class="qa-footer__song">Cry</span> <span class="qa-footer__divider">-</span> <span class="qa-footer__artist">Cigarettes after Sex</span>
+                </a>
             </div>
         </div>
     </footer>
 </template>
+
+<style lang="scss" scoped>
+@use "@/styles/functions" as *;
+@use "@/styles/variables" as *;
+
+.qa-footer {
+    $f: &;
+
+    font-family: "Projekt Blackbird";
+    background-color: #000;
+    color: #fff;
+    padding-top: math-clamp(48, 64);
+    margin-top: math-clamp(120, 160);
+
+    .qa-container {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    &__bottom {
+        padding-top: math-clamp(164, 218.666);
+    }
+
+    &__slug {
+        font-size: math-clamp(64, 85.333);
+    }
+
+    &__copyright {
+        font-size: math-clamp(19, 25.333);
+    }
+
+    &__link {
+        color: #fff;
+        text-decoration: none;
+        display: flex;
+        align-items: center;
+        gap: math-clamp(12, 16);
+        font-size: math-clamp(19, 25.333);
+    }
+}
+</style>
