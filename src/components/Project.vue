e9e43cb51e1e83d664b1c8302d665161a95f5df7
Rohit Jethoe <rohitjethoe@proton.me>
2024-11-11 11:14:55 +0100

    opacity effect

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,14 +1,30 @@
 <script setup>
-import { defineProps, onMounted } from 'vue';
+import { defineProps, onMounted, ref } from 'vue';
 
 const props = defineProps({
     projectTitle: String,
     projectAsset: String
 })
+
+const projectRef = ref(null);
+
+onMounted(() => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                projectRef.value.style.opacity = '1';
+            }
+        });
+    }, { threshold: 0.333 });
+
+    if (projectRef.value) {
+        observer.observe(projectRef.value);
+    }
+});
 </script>
 
 <template>
-    <section class="qa-project">
+    <section class="qa-project" ref="projectRef">
         <div class="qa-project__image">
             <img class="qa-project__asset" :src="projectAsset" :alt="projectTitle"/>
             <div class="qa-project__textual">
@@ -27,6 +43,8 @@ const props = defineProps({
 
     font-family: "Projekt Blackbird";
     margin-bottom: math-clamp(30, 40);
+    opacity: 0.1;
+    transition: 250ms ease-in opacity;
 
     @media (max-width: $sm) {
         margin-bottom: math-clamp(15, 20);
