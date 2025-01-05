dfc34578e6a9e22f1ded249ef6de7e5f22f2a88a
Rohit Biseswar <r.k.biseswar@student.tudelft.nl>
2025-01-05 22:48:17 +0100

    Textual

--- src/static/Introductory.vue
+++ src/static/Introductory.vue
@@ -14,7 +14,7 @@ onMounted(() => {
     <section class="qa-introductory">
       <div class="qa-container">
         <div class="qa-introductory__slug">
-          Crafting pixel-perfect digital experiences, as a full-stack developer besides studying mathematics & engineering methods.        
+          Crafting pixel-perfect digital experiences, as a full-stack developer besides studying computer science & mathematics.        
         </div>
         <div class="qa-introductory__major">
           (BSc) Computer Science and Engineering at T<span>U</span> Delft<br />
