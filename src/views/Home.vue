e56b9202ba9478c7770edec09d63f2bd7526e3f9
Rohit Biseswar <r.k.biseswar@student.tudelft.nl>
2025-05-21 15:51:19 +0200

    Updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,7 +11,7 @@ import Overview from '@/static/Overview.vue';
     <div class="qa-projects">
         <Project projectAsset="/assets/monostatic-cover.webp" projectTitle="Monostatic Studio" projectColor="#ebfd55" />
         <Project projectAsset="/assets/huistuindesign-cover.webp" projectTitle="Huis&Tuin" projectColor="#efe8db"/>
-        <Project projectAsset="/assets/vadimriver-cover.webp" projectTitle="Vadim River Portfolio" projectColor="#fafafa"/>
+        <Project projectAsset="/assets/vadimriver-cover.webp" projectTitle="Vadim River" projectColor="#fafafa"/>
         <Project projectAsset="/assets/virtufit-cover.webp" projectTitle="VirtuFit Service Form" projectColor="#c0d68f"/>
         <Project projectAsset="/assets/highsun-cover.webp" projectTitle="High Sun Glasses" projectColor="#e8eae7"/>
         <Project projectAsset="/assets/greenfuture-cover.webp" projectTitle="Green Future" projectColor="#36422d"/>
